9a7e32d8824b103b08a9a1eb9a25150b49068439
Alessandro Alves <alvesalessandro.school@gmail.com>
2025-02-25 12:01:00 -0300

    arithmetic operations class

---
+++ operadores_aritméticos.py
@@ -0,0 +1,31 @@
+# Adição
+print(1 +1)
+
+# Subtração
+print(10-2)
+
+# Muktiplicação
+print(4 * 3)
+
+# Divisão
+print(12 / 3)
+
+# Divisão Inteira
+print(12 // 2)
+
+# Módulo
+print(10 % 3)
+
+# Exponenciação
+print(2 ** 3)
+
+produto_1 = 10
+produto_2 = 20
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 * produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 % produto_2)
+print(produto_1 ** produto_2)
