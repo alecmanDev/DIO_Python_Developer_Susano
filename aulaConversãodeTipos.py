9a7a7c4ad1dc0e16eb0f88b75ef2343510152524
Alessandro Alves <alvesalessandro.school@gmail.com>
2025-02-25 09:17:32 -0300

    type conversion class

---
+++ aulaConversãodeTipos.py
@@ -0,0 +1,18 @@
+# Conversão de Float para Inteiro
+preco = 10.30
+print(preco)
+
+preco = int(preco)
+print(preco)
+
+# Conversão de String para número
+preco = "10.50"
+idade = "19"
+
+print(float(preco))
+
+print(int(idade))
+
+# Erro de conversão
+preco = "python"
+print()
