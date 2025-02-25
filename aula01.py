7dad90c6c18d91a64694098329d64f63f94533e0
Alessandro Alves <alvesalessandro.school@gmail.com>
2025-02-25 09:12:17 -0300

    Innitial Commit in Bootcamp Python Developer

---
+++ aula01.py
@@ -0,0 +1,11 @@
+# Primeiro Programa
+
+print("Hello Word")
+print("Estou começando meu curso de Python")
+
+n1 = int(input("Digite o 1º Número: "))
+n2 = int(input("Digite o 2º Número: "))
+
+soma = n1 + n2
+
+print(f"A soma é {soma}")
