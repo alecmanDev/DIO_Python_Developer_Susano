74633e843a49c08cc58ccb7e351db76e69716822
Alessandro Alves <alvesalessandro.school@gmail.com>
2025-02-25 09:16:13 -0300

    added variables and constants class

---
+++ aulaVariáveisConstantes.py
@@ -0,0 +1,15 @@
+# Variáveis podem mudar ao longo do código
+nome = "Alec"
+idade = 19
+
+nome = "Alessandro"
+
+print(nome, idade)
+
+# Variáveis devem seguir o padrão Snake Case (" _ ")
+data_de_nascimento = "10/02/2005"
+
+# Constantes devem ser declaradas totalmente MAÍUSCULAS
+STATES = ["SP", "RJ", "PA", "BA"]
+
+print(STATES)
