c4fe754e86fcd7ff20a0a1f54de93b633ae33fc1
Alessandro Alves <alvesalessandro.school@gmail.com>
2025-02-27 08:59:57 -0300

    assignment operator class

---
+++ Módulo 2 - Sintaxe Básica com Python/operadores_atribuição.py
@@ -0,0 +1,35 @@
+# São operadores utilizados para definir o valor inicial ou sobrescrever um valor de uma váriavel
+
+# Atribução Simples
+saldo = 500
+print(saldo)
+
+# Atribuição com adição
+saldo = 500
+saldo += 200
+print(saldo)
+
+# Atribuição com subtração
+saldo = 500
+saldo -= 200
+print(saldo)
+
+# Atribuição com multiplicação
+saldo = 500
+saldo *= 2
+print(saldo)
+
+# Atribuição com divisão
+saldo = 500
+saldo /= 5
+print(saldo)
+
+# Atribuição com módulo
+saldo = 500
+saldo %= 480
+print(saldo)
+
+# Atribuição com exponenciação
+saldo = 50
+saldo **= 2
+print(saldo)
