489a7e2d27879ac11a092ca8c57f055bba4143b7
Alessandro Alves <alvesalessandro.school@gmail.com>
2025-03-01 10:07:50 -0300

    indentation and blocks class

---
+++ Módulo 2 - Sintaxe Básica com Python/Operadores Básicos/Estrutura Condicionais e Repetição/Indentação_blocos.py
@@ -0,0 +1,9 @@
+def sacar(valor):
+    saldo = 500
+
+    if saldo >= valor:
+        print("valor sacado")
+
+sacar(600)
+
+# A indentação é observada quando há espaços à esquerda para deixar o código mais organizado, e no caso do Python para fazer executar
