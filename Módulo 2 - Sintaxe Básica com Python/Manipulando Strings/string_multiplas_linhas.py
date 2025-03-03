78de32611228e31a81f3ebeda3662abcb8dab1d0
Alessandro Alves <alvesalessandro.school@gmail.com>
2025-03-03 08:37:55 -0300

    triple strings

---
+++ Módulo 2 - Sintaxe Básica com Python/Manipulando Strings/string_multiplas_linhas.py
@@ -0,0 +1,10 @@
+# Strings triplas
+nome = "Alessandro"
+
+mensagem = f"""
+    Olá meu nome é {nome},
+Eu estou aprendendo Python.
+       Essa mensagem tem diferentes recuos.
+"""
+
+print(mensagem)
