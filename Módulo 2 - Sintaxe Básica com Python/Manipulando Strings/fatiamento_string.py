8fa3c7d7aca81cfdadbe16f8ddbbdb9c46512635
Alessandro Alves <alvesalessandro.school@gmail.com>
2025-03-03 08:17:18 -0300

    string slicing

---
+++ Módulo 2 - Sintaxe Básica com Python/Manipulando Strings/fatiamento_string.py
@@ -0,0 +1,22 @@
+nome = "Alessandro Alves da Silva"
+
+print(nome[0])
+# >>> A
+
+print(nome[:10])
+# >>> Alessandro
+
+print(nome[11:])
+# >>> Alves da Silva
+
+print(nome[11:16])
+# >>> Alves
+
+print(nome[:10:2])
+# >>> Aesnr
+
+print(nome[:])
+# >>> Alessandro Alves da Silva
+
+print(nome[::-1])
+# >>> avliS ad sevlA ordnasselA
