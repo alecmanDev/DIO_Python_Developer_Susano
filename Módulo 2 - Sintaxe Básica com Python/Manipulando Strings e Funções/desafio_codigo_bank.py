22b38ebd191c3b0862301665d42b4c0785786ef0
Alessandro Alves <alvesalessandro.school@gmail.com>
2025-03-04 23:22:41 -0300

    command code system bank chanllenge

---
+++ Módulo 2 - Sintaxe Básica com Python/Manipulando Strings e Funções/desafio_codigo_bank.py
@@ -0,0 +1,18 @@
+'''  CRIAR UM SISTEMA BANCÁRIO COM AS OPERAÇÕES: SACAR, DEPOSITAR E VISUALIZAR EXTRATO  '''
+
+# Operações de depósito #
+'''
+    Deve ser possível depositar valores positivos para minha conta bancária. A v1 do projeto trabalha apenas com 1 usuário, dessa forma não precisamos nos preocupar em identificar qual é o número da agência e conta bancária. Todo os depósitos devem ser armazenados em uma variável e exibidos em uma operação de extrato.
+'''
+
+# Operação de saque #
+'''
+    O sistema deve permitir realizar 3 saques diários com limite máximo de R$ 500,00 por saque. Caso o usuário não tenha saldo em conta, o sistema deve exibir uma mensagem informando que não será possível sacar o dinheiro por falta de saldo. Todos os saques devem ser armazenados em uma variável e exibidos na operação de extrato.
+'''
+
+# Operação de vizualizar extrato #
+'''
+    Essa operação deve listar todos os depósitos e saques realizados na conta. No fim da listagem deve ser exibido o saldo atual da conta.
+    Os valores devem ser exibidos utilizando o formato R$ xxx.xx, exemplo:
+    1500.45 = R$ 1500,45
+'''
