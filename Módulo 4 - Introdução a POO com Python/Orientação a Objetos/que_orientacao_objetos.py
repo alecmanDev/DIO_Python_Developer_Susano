e72763d512158f9106fd5c44af6336763da2741a
Alessandro Alves <alvesalessandro.school@gmail.com>
2025-03-19 10:03:57 -0300

    class introduction a POO

---
+++ Módulo 4 - Introdução a POO com Python/Orientação a Objetos/que_orientacao_objetos.py
@@ -0,0 +1,12 @@
+# O que é Orientação a Objetos?
+
+# Paradigmas de programação
+'''
+Problema: Beber água
+Solução 1: Usar um copo para beber água
+Solução 2: Usar uma garrafa para beber água
+'''
+
+# Imperativo ou procedural
+# Funcional
+# Orientado a eventos
