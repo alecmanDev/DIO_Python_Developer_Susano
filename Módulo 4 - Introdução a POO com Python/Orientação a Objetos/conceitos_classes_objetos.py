f921d59c7fba1e20a1a6fc7bf6a2d172c63b0142
Alessandro Alves <alvesalessandro.school@gmail.com>
2025-03-19 10:13:38 -0300

    class class and objects

---
+++ Módulo 4 - Introdução a POO com Python/Orientação a Objetos/conceitos_classes_objetos.py
@@ -0,0 +1,18 @@
+# Classes e Objetos
+
+'''
+1️⃣ O que é uma Classe?
+Uma classe é um modelo (molde) que define as características (atributos) e comportamentos (métodos) de um objeto.
+
+📌 Pense na classe como uma planta de um prédio:
+
+A planta define a estrutura (andares, portas, janelas), mas não é o prédio real.
+Vários prédios podem ser construídos a partir da mesma planta (assim como vários objetos podem ser criados a partir de uma classe).
+
+2️⃣ O que é um Objeto?
+Um objeto é uma instância de uma classe, ou seja, uma entidade real criada a partir do modelo da classe.
+
+📌 Pense no objeto como um carro real:
+
+Um "Carro" é uma classe, mas um "Carro da marca Toyota, modelo Corolla, ano 2022" é um objeto específico dessa classe.
+'''
