efd5d69312fb14c92c3ca586a60583fd1efaf937
Alessandro Alves <alvesalessandro.school@gmail.com>
2025-03-20 11:54:27 -0300

    inheritance in OOP

---
+++ Módulo 4 - Introdução a POO com Python/Herança em POO/heranca_poo.py
@@ -0,0 +1,21 @@
+# Herança em POO
+
+'''
+Em programação herança é a capacidade de uma classe filha deriva ou herdar as características e comportamentos da classe pai (base)
+'''
+
+class A:
+    pass
+
+class B(A):
+    pass
+
+# Herança Simples ou Herança Múltipla
+class A:
+    pass
+
+class B:
+    pass
+
+class C(A, B):
+    pass
