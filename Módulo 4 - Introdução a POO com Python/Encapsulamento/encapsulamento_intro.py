69b0f3cfb25da0d1b17788ff902cf618ec9cccdc
Alessandro Alves <alvesalessandro.school@gmail.com>
2025-04-02 09:59:50 -0300

    introduction to encapsulation

---
+++ Módulo 4 - Introdução a POO com Python/Encapsulamento/encapsulamento_intro.py
@@ -0,0 +1,11 @@
+# O que é encapsulamento?
+
+'''
+🔐 Encapsulamento em POO
+Encapsulamento é um dos princípios fundamentais da Programação Orientada a Objetos (POO). Ele consiste em restringir o acesso direto a certos atributos e métodos de uma classe, permitindo que a manipulação dos dados aconteça apenas por meio de métodos específicos.
+
+🎯 Por que usar encapsulamento?
+✅ Proteção de dados – evita modificações acidentais ou mal-intencionadas.
+✅ Melhor organização do código – separa a lógica interna da classe de sua interface externa.
+✅ Facilidade de manutenção – mudanças internas não afetam o código externo que usa a classe.
+'''
